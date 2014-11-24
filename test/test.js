efcc5770701b054cd7a17421564bd742a6e1ae43
Kevin Martensson <kevinmartensson@gmail.com>
2014-11-25 00:36:59 +0100

    Add generated test fixtures

---
+++ test/test.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var creditCardRegex = require('../');
+var test = require('ava');
+
+test('match credit card numbers', function (t) {
+	var fixtures = [].concat(
+		require('./fixtures/americanexpress.json'),
+		require('./fixtures/dinersclub.json'),
+		require('./fixtures/discover.json'),
+		require('./fixtures/mastercard.json'),
+		require('./fixtures/visa.json')
+	).map(function (fixture) {
+		return String(fixture.CreditCard.CardNumber);
+	});
+
+	fixtures.forEach(function (el) {
+		if (!creditCardRegex().exec(el)) {
+			t.assert(false, el);
+			return;
+		}
+
+		t.assert(creditCardRegex().exec(el)[0] === el, el);
+	});
+
+	t.end();
+});
