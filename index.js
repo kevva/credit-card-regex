f83281fa5c275ec39c7e76c061ad4512afa1074d
Kevin Martensson <kevinmartensson@gmail.com>
2019-01-21 02:30:15 +0100

    Require Node.js 8

--- index.js
+++ index.js
@@ -7,10 +7,7 @@ const maestro = '(?:(?:5[0678]\\d\\d|6304|6390|67\\d\\d)\\d{8,15})';
 const mastercard = '(?:(?:5[1-5][0-9]{2}|222[1-9]|22[3-9][0-9]|2[3-6][0-9]{2}|27[01][0-9]|2720)[0-9]{12})';
 const visa = '(?:4[0-9]{12})(?:[0-9]{3})?';
 
-const generate = pattern => opts => {
-	opts = opts || {};
-	return opts.exact ? new RegExp(`(?:^${pattern}$)`) : new RegExp(pattern, 'g');
-};
+const generate = pattern => (opts = {}) => opts.exact ? new RegExp(`(?:^${pattern}$)`) : new RegExp(pattern, 'g');
 
 module.exports = generate([
 	americanExpress,
